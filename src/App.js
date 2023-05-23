5bd198dd8f2826c111fbe02bd757d98f749acab9
suryacodess <helloosuryaprakash@gmail.com>
2023-05-23 17:52:46 +0530

    updated app.js and css

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("search");
   const searchQ = useRef();
   const searchGif = () => {
+    const val = searchQ.current.value;
+    // if("search" === searchQuery ){
+    //   alert("enter your query to get gifs.")
+    // }
+
+    // else{
     console.log(searchQuery);
-    setSearchQuery(searchQ.current.value);
+    setSearchQuery(val);
+
+    // }
   };
 
+  const limit = 20;
+  const stickers = `https://api.giphy.com/v1/stickers/search?api_key=xil6kRlrRdGdYWb8OpOwxwgO9PsuU0wJ&q=${searchQuery}&limit=${limit}&offset=0&rating=pg&lang=en`;
+  const gifs = `https://api.giphy.com/v1/gifs/search?api_key=xil6kRlrRdGdYWb8OpOwxwgO9PsuU0wJ&q=${searchQuery}&limit=${limit}&offset=0&rating=pg&lang=en`;
+
   useEffect(() => {
-    fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=xil6kRlrRdGdYWb8OpOwxwgO9PsuU0wJ&q=${searchQuery}&limit=25&offset=0&rating=g&lang=en`
-    )
+    fetch(gifs)
       .then((Response) => Response.json())
       .then((response) => {
         // console.log(response.data);
@@ -33,14 +43,21 @@ function App() {
         </div>
       </header>
       <main className="gif-section">
-        {/*  */}
-        {gif.map((gifs) => {
-          return (
-            <>
-              <img key={Math.random()} src={gifs.images.original.url} alt={searchQuery}></img>
-            </>
-          );
-        })}
+        <div className="gif-inner">
+          {gif.map((gifs) => {
+            return (
+              <>
+                <div className="gif-img">
+                  <img
+                    key={Math.random()}
+                    src={gifs.images.original.url}
+                    alt={searchQuery}
+                  ></img>
+                </div>
+              </>
+            );
+          })}
+        </div>
       </main>
     </>
   );
