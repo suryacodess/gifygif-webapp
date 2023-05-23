c4003164af1f5fc1a98313590c798f5037633cc5
suryacodess <helloosuryaprakash@gmail.com>
2023-05-23 17:01:32 +0530

    added giphy api call and search functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./components/sass/style.css";
 
 function App() {
+  const [gif, setGif] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("search");
+  const searchQ = useRef();
+  const searchGif = () => {
+    console.log(searchQuery);
+    setSearchQuery(searchQ.current.value);
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=xil6kRlrRdGdYWb8OpOwxwgO9PsuU0wJ&q=${searchQuery}&limit=25&offset=0&rating=g&lang=en`
+    )
+      .then((Response) => Response.json())
+      .then((response) => {
+        // console.log(response.data);
+        setGif(response.data);
+      });
+  }, [searchQuery]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="title">
+          <h1>GifyGif</h1>
+        </div>
+        <div className="gif-searchbar">
+          <input type="text" ref={searchQ} placeholder="search" />
+          <button onClick={() => searchGif(searchQuery)}>search</button>
+        </div>
       </header>
-    </div>
+      <main className="gif-section">
+        {/*  */}
+        {gif.map((gifs) => {
+          return (
+            <>
+              <img key={Math.random()} src={gifs.images.original.url} alt={searchQuery}></img>
+            </>
+          );
+        })}
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Gif.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sass/style.css";
+
+
+export default function Gif() {
+  return (
+    <>
+      <main className="gif-section">
+        {/* <img src="https://media1.giphy.com/media/BCgcfnaGDaYai5eYD1/giphy.gif?cid=48194affwp7kmi2ls9h488u9ubpr6re6q4bucfe3rxuwc455&ep=v1_gifs_search&rid=giphy.gif&ct=g"></img> */}
+      </main>
+    </>
+  );
+}
